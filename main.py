0ad6a266b3725dac3a25479a0079422ef2a3a31f
nurzhan <nurzhan.erezhep@gmail.com>
2021-06-11 10:59:02 +0600

    correct the error digits in '/'

--- main.py
+++ main.py
@@ -74,9 +74,10 @@ class RequestAPI:
 
 @app.get('/')
 def home_page():
+    digits = datasets.load_digits()
     clf = svm.SVC(gamma=0.001, C=100.)
     clf.fit(digits.data[:-1], digits.target[:-1])
-    prediction = clf.predict(digits.data[-1:])
+    prediction = str(clf.predict(digits.data[-1:]))
     """return 'Hi this my first project in Heroku ' \
            'you can use two link: /countries/ and /quotes/ '"""
     return prediction
